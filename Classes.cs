c1a9b5922b1b1587c567af7c90dd030d079fea3c
Dallin Chipman <dallinchipman@gmail.com>
2017-02-02 18:19:24 -0700

    DGM1600 Classes

---
+++ Classes.cs
@@ -0,0 +1,20 @@
+// a set or category of things having some property or attribute in common and differentiated from others by kind, type, or quality.
+
+public class classes : MonoBehavior
+{
+    int score = 0;
+}
+
+public class guns
+{
+    public int glock = 2
+    public int p90 = 1
+    public int barret = 3
+}
+
+public class arrows
+{
+    public int fireArrows = 6
+    public int electricArrows = 6
+    public int explosiveArrows = 6
+}
