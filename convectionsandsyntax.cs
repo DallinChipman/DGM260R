78deb33eda8c1778ebd39b24e8dc74eefc2bad68
Dallin Chipman <dallinchipman@gmail.com>
2017-02-02 18:54:44 -0700

    DGM1600 Convection and Syntax

---
+++ convectionsandsyntax.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start ()
+    {
+        Debug.Log(transform.position.y);
+        
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+
+public class BasicSyntax 
+{
+    void Start ()
+    {
+        Debug.Log(transform.position.x);
+        
+        if(transform.position.x <= 10f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+
+public class BasicSyntax
+{
+    void Start ()
+    {
+        Debug.Log(transform.position.z);
+        
+        if(transform.position.z <= 25f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+
+public class BasicSyntax 
+{
+    void Start ()
+    {
+        Debug.Log(transform.position.y);
+        
+        if(transform.position.y <= 50f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+}
+
+public class BasicSyntax 
+{
+    void Start ()
+    {
+        Debug.Log(transform.position.x);
+        
+        if(transform.position.x <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+}
