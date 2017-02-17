936faa09cbcbad305e1ff6bed4bbdc798460bc98
Dallin Chipman <dallinchipman@gmail.com>
2017-02-16 18:11:28 -0700

    switch statements

---
+++ switch.cs
@@ -0,0 +1,68 @@
+public class ConversationScript : MonoBehavior
+{
+    public int intelligence = 5;
+
+    void Greet()
+    {
+        switch (intelligence)
+        {
+        case 5:
+            print ("why hello there good sir: let me teach you about Trigonometry")
+            break;
+        case 4:
+         print ("hows it going?")
+            break;
+        case 3:
+            print ("Hello")
+            break;
+        case 2:
+         print ("How come?")
+            break;
+        case 1:
+         print ("Yes we can.")
+            break;
+}
+public int wizard = 25;
+
+    void Greet()
+    {
+        switch (wizard)
+        {
+        case 5:
+            print ("cast a spell")
+            break;
+        case 4:
+         print ("help us")
+            break;
+        case 3:
+            print ("right now")
+            break;
+        case 2:
+         print ("he is good")
+            break;
+        case 1:
+         print ("he is evil.")
+            break;
+}
+public int cupsinthesink = 5;
+
+    void Greet()
+    {
+        switch (cupsinthesink)
+        {
+        case 5:
+            print ("how many cups?")
+            break;
+        case 4:
+         print ("why aren't they in the sink")
+            break;
+        case 3:
+            print ("10 of them")
+            break;
+        case 2:
+         print ("I've washed a cup")
+            break;
+        case 1:
+         print ("I haven't washed a cup")
+            break;
+}
