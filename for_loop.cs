010a53a9bcc73c370a1807b62a893bb8d6382617
Dallin Chipman <dallinchipman@gmail.com>
2017-03-23 20:43:49 -0600

    homework

---
+++ for_loop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class for_loop : Monobehaviour {
+
+    // Use this for initialization 
+    void Start () {
+    
+    {
+    
+    //Update is called once per frame
+    void update () {
+        public int bottles = 0;
+
+        for (int i=99;i>bottles;i--)
+            print(i+"bottles of apple beer on the wall");
+        }
+    }
+}
